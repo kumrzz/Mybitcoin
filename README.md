2445d34f0405189f825dbb1ddad25c1c439e3d23
Kumar Ghosh <kumrzz@gmail.com>
2016-05-09 14:42:42 +0100

    updated commentary

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ https://github.com/Shultzi/Mybitcoin/blob/master/connection.py
 https://docs.python.org/2/library/struct.html  
 https://en.bitcoin.it/wiki/Protocol_documentation  
 https://blockchain.info/connected-nodes  
-use wireshark to see connection.py at work  
+use wireshark(winpcap required!) to see connection.py at work  
 https://docs.python.org/2/library/socket.html  
  
 2. keys and addresses  
@@ -15,6 +15,7 @@ https://www.youtube.com/watch?v=tX-XokHf_nI
  
 3.
 
+https://en.bitcoin.it/wiki/Testnet  
 notes: to setup testnet, 
 C:\Users\usernamehere\AppData\Roaming\Bitcoin\bitcoin.conf shd contain  
 testnet=1  
--- connection.py
+++ connection.py
@@ -5,6 +5,8 @@ import hashlib
 import socket
 import os
 
+#below edited(coz I am in testnet) per windows command:
+# C:\Progra~1\Bitcoin\daemon\bitcoin-cli getpeerinfo
 version = struct.pack("L", 70002)
 
 services = struct.pack("Q", 0)
@@ -15,27 +17,31 @@ addr_recv_services = struct.pack("Q", 0) #services
 
 addr_recv_ip = struct.pack(">16s", "127.0.0.1")
 
-addr_recv_port = struct.pack(">H", 8333)
+#below edited from 8333(coz I am in testnet)
+addr_recv_port = struct.pack(">H", 18333)
 
 addr_trans_services = struct.pack("Q", 0) #services
 
 addr_trans_ip = struct.pack(">16s", "127.0.0.1")
 
-addr_trans_port = struct.pack(">H", 8333)
+#below edited from 8333(coz I am in testnet)
+addr_trans_port = struct.pack(">H", 18333)
 
 nonce = struct.pack("Q", random.getrandbits(64))
 
+#not updating agent as this is fine @ zero
 user_agent_bytes = struct.pack("B", 0)
 
-#below two edited(coz I am in testnet) per bitcoin-cli getpeerinfo
-# inside C:\Program Files\Bitcoin\daemon\
-starting_height = struct.pack("L", 827515)
+#below edited(coz I am in testnet) per windows command:
+# C:\Progra~1\Bitcoin\daemon\bitcoin-cli getpeerinfo
+starting_height = struct.pack("L", 827541)
 
 relay = struct.pack("?", False)
 
 payload = version + services + timestamp + addr_recv_services + addr_recv_ip + addr_recv_port + addr_trans_services + addr_trans_ip + addr_trans_port + nonce + user_agent_bytes + starting_height + relay
 
-magic = "F9BEB4D9".decode("hex")
+#magic = "F9BEB4D9".decode("hex") coz I am in testnet:
+magic = "0B110907".decode("hex")
 
 command = "version" + 5 * "\00"
 
@@ -47,9 +53,9 @@ msg = magic + command + length + check + payload
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-#below two edited(coz I am in testnet) per bitcoin-cli getpeerinfo
-# inside C:\Program Files\Bitcoin\daemon\
-HOST = "185.24.221.107"
+#below two edited(coz I am in testnet) per windows command:
+# C:\Progra~1\Bitcoin\daemon\bitcoin-cli getpeerinfo
+HOST = "137.116.160.176"
 PORT = 18333
 
 s.connect((HOST, PORT))
